a22352cb05839970e23ee85fc272022d39f7694e
Adarsh Kumar <adarshku123456789@gmail.com>
2025-10-15 21:52:30 +0530

    feat: add loading spinner to DashboardLayout during lazy loading

--- app/(main)/dashboard/layout.js
+++ app/(main)/dashboard/layout.js
@@ -1,12 +1,13 @@
 import React, { Suspense } from 'react'
 import DashboardPage from './page';
+import BarLoader from 'react-spinners'
 
 const DashboardLayout = () => {
   return (
     <div className='px-5'>
       <h1 className='text-6xl font-bold gradient-title mb-5'>Dashboard</h1>
       
-      <Suspense fallback={}>
+      <Suspense fallback={<BarLoader/>}>
         <DashboardPage />
       </Suspense>
 
