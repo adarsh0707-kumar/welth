e90435d700a86bd9b75ba542d60d03b7503927f2
Adarsh Kumar <adarshku123456789@gmail.com>
2025-10-15 22:09:48 +0530

    Adding text in page of dashboard

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -3,8 +3,12 @@ import React from 'react'
 const DashboardPage = () => {
   return (
     <div className='px-5'>
-      
-      
+      {/* Budget Progress */}
+      page
+
+      {/* Overview */}
+
+      {/* Account Grid */}
 
     </div>
   )
