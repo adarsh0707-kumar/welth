5f3e8e4f27f2ccd0f20ba15311a01324e411df8e
Adarsh Kumar <adarshku123456789@gmail.com>
2025-10-15 22:24:48 +0530

    Dashboard Server

--- action/dashboard.js
+++ action/dashboard.js
@@ -2,6 +2,15 @@
 
 import { db } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server"
+import { revalidatePath } from "next/cache";
+
+const serializeTransaction = (obj) => {
+  const serialized = { ...obj };
+
+  if (obj.balance) {
+    serialized.balance = obj.balance.toNumber();
+  }
+}
 
 export async function createAccount(data) {
 
@@ -12,8 +21,59 @@ export async function createAccount(data) {
     const user = await db.user.findUnique({
       where: { clerkUserId: userId }
     });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    // convert balance to float before saving
+
+    const balanceFloat = parseFloat(data.balance)
+
+    if (isNaN(balanceFloat)) {
+      throw new Error("Invalid balance amount")
+
+    }
+
+    // check if this is the user's first account
+
+    const existingAccounts = await db.account.findMany({
+      where:{userId: user.id}
+    })
+
+    const shouldBeDefault = existingAccounts.length === 0 ? true : data.isDefault;
+
+    // if this account should be default, unset other default acoounts
+
+    if (shouldBeDefault) {
+      awaitdb.account.updateMany({
+        where: { userId: user.id, isDefault: true },
+        data: { isDefault: false },
+      });
+    }
+
+    const account = await db.account.create({
+      data: {
+        ...data,
+        balance: balanceFloat,
+        userId: user.id,
+        isDefault: shouldBeDefault,
+      },
+    });
+
+    const serializedAccount = serializeTransaction(account);
+
+    revalidatePath("/dashboard")
+
+    return {
+      success: true,
+      data: serializedAccount,
+    }
+
+
   }
   catch (err) {
-    console.error(err)
+    console.error(err.message)
+    throw new Error(err.message)
   }
 }
