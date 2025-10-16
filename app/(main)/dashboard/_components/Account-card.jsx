1af8ab77ee3ec3f06e489dff9904b781773056d2
Adarsh Kumar <adarshku123456789@gmail.com>
2025-10-16 19:43:34 +0530

    Make account default

--- app/(main)/dashboard/_components/Account-card.jsx
+++ app/(main)/dashboard/_components/Account-card.jsx
@@ -1,5 +1,11 @@
-import React from 'react'
+"use client"
+import Link from 'next/link'
+import { toast } from 'sonner'
+import React, { useEffect } from 'react'
+import useFeatch from '@/hooks/use-featch'
 import { Switch } from '@/components/ui/switch'
+import { updateDefaultAccount } from '@/action/accounts'
+import { ArrowDownRight, ArrowUpRight } from 'lucide-react'
 import {
   Card,
   CardContent,
@@ -7,12 +13,41 @@ import {
   CardHeader,
   CardTitle
 } from '@/components/ui/card'
-import { ArrowDownRight, ArrowUpRight } from 'lucide-react'
-import Link from 'next/link'
 
 const AccountCard = ({ account }) => {
 
   const { name, type, balance, id, isDefault } = account
+
+  const {
+    loading: updateDefaultLoading,
+    fn: updateDefaultFn,
+    data: updatedAccount,
+    error,
+  } = useFeatch(updateDefaultAccount)
+
+  const handleDefaultChange = async (event) => {
+    event.preventDefault();
+
+    if (isDefault) {
+      toast.warning("You need atleast 1 default account");
+      return;
+    }
+
+    await updateDefaultFn(id);
+  }
+
+  useEffect(() => {
+    if (updatedAccount?.success) {
+      toast.success("Default account updated successfully");
+    }
+  },[updatedAccount, updateDefaultLoading])
+    
+  useEffect(() => {
+    if (error) {
+      toast.error(error.message || "Failed to update default account");
+    }
+  }, [error]);
+
 
 
   return (
@@ -20,7 +55,12 @@ const AccountCard = ({ account }) => {
       <Card className="hover:shadow-md transition-shadow group relative">
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
           <CardTitle className='text-sm font-medium capitalize'>{name}</CardTitle>
-          <Switch checked={isDefault } className="cursor-pointer" />
+          <Switch
+            checked={isDefault}
+            className="cursor-pointer"
+            onClick={handleDefaultChange}
+            disavled={updateDefaultLoading}
+          />
         </CardHeader>
         <CardContent>
           <div className='text-2xl font-bold'>
