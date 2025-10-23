51bcd6e4acdd9a1f617dff0d39bb57d5e6e2c2d5
Adarsh Kumar <adarshku123456789@gmail.com>
2025-10-23 10:36:44 +0530

    feat: add monthly report generation and financial insights functionality

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,6 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
-import { checkBudgetAlert, processRecurringTransaction, triggerRecurringTransactions } from "@/lib/inngest/functions";
+import { checkBudgetAlert, generateMonthlyReports, processRecurringTransaction, triggerRecurringTransactions } from "@/lib/inngest/functions";
 
 // Create the API handler with functions properly registered
 export const { GET, POST, PUT } = serve({
@@ -8,6 +8,7 @@ export const { GET, POST, PUT } = serve({
   functions: [
     checkBudgetAlert, 
     triggerRecurringTransactions,
-    processRecurringTransaction
+    processRecurringTransaction,
+    generateMonthlyReports
   ],
 });
