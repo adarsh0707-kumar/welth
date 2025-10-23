a4bac01e0cff54931b2c3d637fd770b8a07fd369
Adarsh Kumar <adarshku123456789@gmail.com>
2025-10-24 00:49:20 +0530

    feat: integrate arcjet middleware into clerk middleware for enhanced route protection

--- middleware.js
+++ middleware.js
@@ -1,12 +1,33 @@
 // middleware.js
-import arcjetMiddleware from "./middleware.arcjet";
-import clerkMiddleware from "./middleware.clerk";
+import { clerkMiddleware } from "@clerk/nextjs/server";
+import arcjet, { createMiddleware, shield, detectBot } from "@arcjet/next";
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE", "GO_HTTP"],
+    }),
+  ],
+});
+const arcjetMiddleware = createMiddleware(aj);
+
+export default clerkMiddleware(async (auth, req, ev) => {
+  // run Arcjet middleware first
+  await arcjetMiddleware(req, ev);
+
+  const { userId, redirectToSignIn } = await auth();
+  const protectedRoutes = ["/dashboard", "/account", "/transaction"];
+  if (
+    !userId &&
+    protectedRoutes.some((r) => req.nextUrl.pathname.startsWith(r))
+  ) {
+    return redirectToSignIn();
+  }
+});
 
 export const config = {
-  matcher: ["/((?!_next|.*\\..*).*)", "/(api|trpc)(.*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
 };
-
-export default async function middleware(req, ev) {
-  await arcjetMiddleware(req, ev);
-  await clerkMiddleware(req, ev);
-}
